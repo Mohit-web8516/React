fed3dc923451410b4e7069150bc5feac35d1c645
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-15 13:11:39 +0530

    app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,17 @@
 import Viral from "./viral"
 
 function App() {
+  const username = "letsss learn react"
   return (
-    <div> 
+    // <div>
+    <>
+
 
       <Viral />
-      <h1>letsss learn react</h1>
+      <h1>letsss learn react {username}</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad beatae dolore earum iusto accusantium inventore ratione, voluptatem nam eveniet! Ipsum obcaecati perspiciatis corrupti ad esse enim optio autem, eligendi laborum.</p>
-    </div>
+    </>
+    // </div>
   )
 }
 
