649b0883180b802ba775105c8cad5f63b540de14
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-15 11:56:49 +0530

    app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,7 +2,12 @@ import Viral from "./viral"
 
 function App() {
   return (
-    <Viral />
+    <div> 
+
+      <Viral />
+      <h1>letsss learn react</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad beatae dolore earum iusto accusantium inventore ratione, voluptatem nam eveniet! Ipsum obcaecati perspiciatis corrupti ad esse enim optio autem, eligendi laborum.</p>
+    </div>
   )
 }
 
