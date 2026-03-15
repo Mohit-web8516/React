c9d2f8efdc09e0050a27e555d1d07f1246434dfb
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-15 20:35:42 +0530

    added

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = () => {
+    // counter = counter + 1
+    setCounter(counter + 1)
+    // console.log("clicked", counter);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+
+      <h1>hello !! lets learn react...</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>add value{counter} </button>
+      <br />
+      <button onClick={removeValue} >remove value{counter}</button>
+      <p>footer: {counter}</p>
+    </>
+
+  )
+
+}
+
+export default App
