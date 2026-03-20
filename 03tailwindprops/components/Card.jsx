9a1cd6cf255bb4f1e398394f47c9dc198548e9da
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-20 15:05:49 +0530

    card

--- 03tailwindprops/components/Card.jsx
+++ 03tailwindprops/components/Card.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react';
 
-function Card() {
+function Card({ username, btnText = "visit me" }) {
+    console.log(username);
     return (
-
-        <div>Card</div>
-    )
-
+        <div className="relative h-[400px] w-[300px] rounded-md ">
+            <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?..."
+                alt="AirMax Pro"
+                className="z-0 h-full w-full rounded-md object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+            <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">{username}</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                    {btnText} →
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default Card
+export default Card;
