c8e38327cce58c1696a132890cdd647bb5613fd5
mohit-8516 <mohitshrivastav429@gmail.com>
2026-03-20 15:06:02 +0530

    app

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';   // or '/vite.svg' if in public
+import './App.css';
+import Card from '../components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-  let myObj = {
-    username: "hitesh",
-    age: 21
-  }
-  let newArr = [1, 2, 3]
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="chaiaurcode" btnText="click me" />
-      <Card username="hitesh" />
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="coffee with you" btnText="click me" />
+      <Card username="Baby Gurlllll" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
